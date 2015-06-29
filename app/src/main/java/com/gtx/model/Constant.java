8390f7e054baeb9987c7fb61b27bc500841b6c6a
gtx1049 <gtx_122@126.com>
2015-06-29 21:38:28 +0800

    add constant

---
+++ app/src/main/java/com/gtx/model/Constant.java
@@ -0,0 +1,12 @@
+package com.gtx.model;
+
+/**
+ * Created by Administrator on 2015/6/29.
+ */
+public class Constant
+{
+    public final static String SHAKETOEAT = "shaketoeat";
+    public final static String ISFIRST = "isfirst";
+
+    public final static String CREATE_TABLE = "create table stoe(canteen varchar(255), dish varchar(255))";
+}
